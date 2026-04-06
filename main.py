d1a8004c22ab20e9a2bf1fb1e68a9f4429f2310d
Arpan Shrestha <arpanshrestha12@gmail.com>
2026-04-06 11:43:21 +0545

    Wrapped with gradio UI for minimal look

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import whisper
+import gradio as gr
+import os
+
 model = whisper.load_model("small")
-result = model.transcribe("eps82.mp3", fp16=False)
 
-with open("transcript.txt", "w", encoding="utf-8") as f:
-    f.write(result["text"])
-print("Transcript saved!!!")
 
+def transcript_audio(file):
+    result = model.transcribe(file, fp16=False)
+    
+    filename = os.path.splitext(file.name)[0]
+    output_file=f"{filename}_transcript.txt"
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write(result["text"])
+    return result["text"], output_file
+
+app=gr.Interface(
+    fn=transcript_audio,
+    inputs=gr.File(type="filepath", label='Upload mp3/wav'),
+    outputs=[
+        gr.Textbox(label="Transcript"),
+        gr.File(label="Download Transcript")
+    ],
+    title="Whisper Transcript Generator",
+    description="Upload audio and get transcript"
+)
+app.launch()
